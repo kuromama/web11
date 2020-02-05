745ac9a88cd19a90edc492099a76098a3233958a
kuromama <chenonwork@gmail.com>
2020-02-05 13:30:28 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'head.php';
+
+echo $_SESSION['admin'];die();
+ //看自己倒底有沒有登入  
+  //如果變1就是管理員
+  //如果空白就是沒有登入
+
+
+/*---- 程式結尾-----*/
+$smarty->display('theme.tpl');
---
+++ templates_c/de7758dc734a79cea563dc75561e601d126d76f8_0.file.admin.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-02-05 05:07:09
+  from 'D:\teacherguo\ugm\xampp\htdocs\web11\templates\tpl\admin.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5e3a3f6d2f2041_93288584',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'de7758dc734a79cea563dc75561e601d126d76f8' => 
+    array (
+      0 => 'D:\\teacherguo\\ugm\\xampp\\htdocs\\web11\\templates\\tpl\\admin.tpl',
+      1 => 1580875269,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e3a3f6d2f2041_93288584 (Smarty_Internal_Template $_smarty_tpl) {
+?><h1>圓圓電腦工作室後台</h1>
+<?php }
+}
